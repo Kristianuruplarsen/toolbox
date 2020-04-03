27c17c293b489d134006677a6ee48fca9e0f95b7
Kristian Urup Olesen Larsen <kristianuruplarsen@gmail.com>
2020-04-03 13:48:41 +0200

    remove notebook and update

--- toolbox/matplotlib/matplotlibtools.py
+++ toolbox/matplotlib/matplotlibtools.py
@@ -2,9 +2,39 @@
 ''' General tools for working with matplotlib
 '''
 
-
+import numpy as np
 from functools import wraps
 import matplotlib.pyplot as plt 
+
+def integer_ticks(data, spacing = 1):
+    ''' Construct integer tick labels from 
+        a sequence of data.
+    '''
+    return np.arange(np.floor(np.min(data)),
+                     np.ceil(np.max(data)) + 1,
+                     spacing)
+
+
+def adjust_spines(ax, spines = ['left', 'bottom']):
+    ''' Adjust the spines of an axis to have 
+        spacing between them where they meet.
+    '''
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 10))
+            spine.set_smart_bounds(True)
+        else:
+            spine.set_color('none')
+    
+    if 'left' in spines:
+        ax.yaxis.set_tick_position('left')
+    else:
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_tick_position('bottom')
+    else:
+        ax.xaxis.set_ticks([])
 
 
 def logx(plot_function):
--- toolbox/pandas/pandastools.py
+++ toolbox/pandas/pandastools.py
@@ -42,3 +42,53 @@ def spread(df, key, value):
     cols = [col for col in df.columns if col not in (key, value)]
     return pd.pivot_table(df, index = cols, columns = key, values = value)\
             .reset_index()
+
+
+
+class Collapse:
+    def __init__(self, df, groups, funcs, columns = None):
+        ''' Context manager that collapses a pd.DataFrame
+            e.g. for plotting.
+
+        Parameters
+        ----------
+        df (pd.DataFrame): the data to collapse.
+        groups (list): the columns to group by.
+        funcs (list): the functions to summarize by.
+        columns (list): the columns to keep in the collapse.
+        '''
+        if isinstance(df, pd.DataFrame):
+            self.df = df 
+        if isinstance(df, pd.Series):
+            self.df = df.reset_index()
+
+        self.groups = self.set_groups(groups)
+        self.funcs = self.set_functions(funcs)
+        self.columns = self.set_columns(columns) if columns is not None else df.columns
+
+        def _set(self, value, error = ''):
+            if isinstance(value, list):
+                return value 
+            elif isinstance(value, tuple):
+                return list(value)
+            elif isinstance(value, str):
+                return [x.strip() for x in value.split(',')]
+            raise ValueError(error)
+
+        def set_groups(self, groups):
+            return self._set(groups, f'Unknown group format {groups}')
+
+        def set_columns(self, columns):
+            return self._set(columns, f'Unknown column format {columns}')
+
+        def set_functions(self, funcs):
+            return self._set(funcs, f'Unknown function format {funcs}')
+
+        def __enter__(self):
+            df = self.df.groupby(self.groups)[self.columns].agg(self.funcs).reset_index()
+            df.columns = ['_'.join([a,b]) if b != '' else a for a,b in zip(df.columns.get_level_values(0), df.columns.get_level_values(1))]
+            print('Columns are: ', ', '.join(df.columns))
+            return df
+
+        def __exit__(self, *a, **kw):
+            return
